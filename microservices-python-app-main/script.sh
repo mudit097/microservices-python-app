8d0e16c153564d96d12ab22d6fcba1d534885efb
Ubuntu <ubuntu@ip-172-31-6-236.ap-south-1.compute.internal>
2023-11-01 18:30:20 +0000

    Your commit message here

--- microservices-python-app-main/script.sh
+++ microservices-python-app-main/script.sh
@@ -1,7 +1,7 @@
-#install docker
+# Install Docker
 sudo apt-get update
 sudo apt-get install docker.io -y
-sudo usermod -aG docker ubuntu  
+sudo usermod -aG docker ubuntu
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
 
@@ -9,7 +9,7 @@ sudo chmod 777 /var/run/docker.sock
 sudo apt install wget -y
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform
+sudo apt update && sudo apt install terraform -y
 
 # Install kubectl
 sudo apt update
@@ -18,39 +18,36 @@ curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 
-# Install AWS CLI 
+# Install AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt-get install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
 
-#install helm
+# Install Helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-#install python
+# Install Python
 sudo apt update
 sudo apt install python3 -y
 
-#install postgresql
-#!/bin/bash
-# Update the package list and install PostgreSQL
+# Install PostgreSQL
 sudo apt update
 sudo apt install -y postgresql postgresql-contrib
 # Start the PostgreSQL service and enable it on boot
 sudo systemctl start postgresql
 sudo systemctl enable postgresql
 
-#mongoDB
+# Install MongoDB
 sudo apt update
 sudo apt install wget curl gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release -y
-curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/mongodb-6.gpg
+curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/mongodb-6.gpg
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt update
 sudo apt install mongodb-org -y
 sudo systemctl enable --now mongod
 sudo systemctl status mongod
 mongod --version
-
 
