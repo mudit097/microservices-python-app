fd0c8b023e20b8998a8c748ceaf889c13a094474
Mudit Mathur <85672841+mudit097@users.noreply.github.com>
2023-11-22 12:21:45 +0530

    Update backend.tf

--- microservices-python-app-main/Eks-terraform/backend.tf
+++ microservices-python-app-main/Eks-terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "mycompany-projectname-uniquestring" # Replace with your actual unique S3 bucket name
 
-    key    = "EKS/terraform.tfstate"
+    key    = "CICD/terraform.tfstate"
     region = "ap-south-1" # Replace with your desired AWS region (e.g., ap-south-1)
   }
 }
